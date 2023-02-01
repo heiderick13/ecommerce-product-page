a853c27fad383e73bd3b1438c73d1963ca966d1e
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-02-01 16:57:51 -0300

    add: hover state dos nav-items || cart-container vazio ok

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,15 @@
 const imgsModal = document.querySelector("#modal-imgs");
 const mainImage = document.querySelector(".main-img");
 const closeModal = document.querySelector(".close");
+const links = document.querySelectorAll("a");
+const linksArr = [...links];
+const productQntDisplay = document.querySelector("#quantity");
+const cartIcon = document.querySelector("#cart-icon");
+const cartContainer = document.querySelector("#cart-container");
+let productQnt = 0;
+
+productQntDisplay.textContent = productQnt;
+// cartIcon.setAttribute("data-value", productQnt); add with function
 
 // Event handlers
 mainImage.addEventListener("click", () => {
@@ -9,4 +18,14 @@ mainImage.addEventListener("click", () => {
 
 closeModal.addEventListener("click", () => {
   imgsModal.close();
+});
+
+linksArr.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+cartIcon.addEventListener("click", () => {
+  cartContainer.classList.toggle("hide-cart");
 });
